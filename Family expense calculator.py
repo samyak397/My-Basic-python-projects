63ec2c26e5c06939e241a302724c635eb88b61d8
samyak397 <samyakp465@gmail.com>
2026-09-10 14:10:32 +0530

    Add files via upload

---
+++ Family expense calculator.py
@@ -0,0 +1,11 @@
+Income = float(input("Write your Income"))
+Rent= float(input("Write your Rent"))
+Groceries=float(input("Write your groceries expense"))
+Remaining_money=float(input("Side expenses"))
+
+print("Total expense is" ,Rent+Groceries+Remaining_money)
+
+print("Remain after rent" , Income-Rent)
+print("Remain after Groceries" , Income-Groceries)
+print("Remain after Side expenses" , Income- Remaining_money)
+print("Remaining money" , Income-Rent-Groceries-Remaining_money)
